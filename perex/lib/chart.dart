30f5fac2f7a1fe835d53f9f02bf0d203a2404aa7
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2023-02-15 20:39:30 +0700

    74

---
+++ perex/lib/chart.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Chart extends StatefulWidget {
+  const Chart({super.key});
+
+  @override
+  State<Chart> createState() => _ChartState();
+}
+
+class _ChartState extends State<Chart> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: const Card(
+        child: Text('Chart'),
+      ),
+    );
+  }
+}
